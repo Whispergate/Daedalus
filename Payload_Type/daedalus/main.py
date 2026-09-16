70eb7a151c25b24f52ba1cfb7d1152709d217002
Lav <l.m@securescape.cc>
2026-09-16 07:55:33 +0200

    Fixed pathing issue

--- Payload_Type/daedalus/main.py
+++ Payload_Type/daedalus/main.py
@@ -1,6 +1,6 @@
 import asyncio
 from mythic_container import mythic_service
 from daedalus.eventing import DaedalusEventing  # noqa: F401 - registers with mythic_service
-from Payload_Type.daedalus.daedalus_ca.builder import DaedalusCA  # noqa: F401 - registers CA with mythic_service
+from daedalus_ca.builder import DaedalusCA  # noqa: F401 - registers CA with mythic_service
 
 asyncio.run(mythic_service.start_and_run_forever())
