b74752fec974ae4d9b4f628a13173b6ca56d4738
Lav <l.m@securescape.cc>
2026-09-20 07:06:18 +0200

    Added the Jenkins Token env variable back

--- Payload_Type/daedalus/main.py
+++ Payload_Type/daedalus/main.py
@@ -1,11 +1,4 @@
 import asyncio
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_file = Path("/Mythic/.env")
-if env_file.exists():
-    load_dotenv(env_file, override=False)
-
 from mythic_container import mythic_service
 from daedalus.eventing import DaedalusEventing  # noqa: F401 - registers with mythic_service
 from daedalus_ca.builder import DaedalusCA  # noqa: F401 - registers CA with mythic_service
